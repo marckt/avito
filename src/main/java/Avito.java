1d71edc5ae45dff73b47074ae5f9a909da81ed87
marckt <marck.for@yandex.ru>
2018-07-10 17:02:21 +0700

    1

--- src/main/java/Avito.java
+++ src/main/java/Avito.java
@@ -5,13 +5,13 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-/*Парсит количество просмотров, если чато парсить уйдет в баню */
+/*Парсит количество просмотров */
 public class Avito {
     private static String[] mySites = {"https://www.avito.ru/novosibirsk/predlozheniya_uslug/maloetazhnoe_stroitelstvo._rekonstruktsiya_doma_799177412",
             "https://www.avito.ru/novosibirsk/posuda_i_tovary_dlya_kuhni/tarelka_1398355167",
             "https://www.avito.ru/novosibirsk/tovary_dlya_kompyutera/prodam_otlichnyy_bolshoy_korpus_powerman_pro_pm-003_1649809101",
             "https://www.avito.ru/novosibirsk/tovary_dlya_kompyutera/hp_g6-2000_zapchasti_1342778071",
-            "https://www.avito.ru/novosibirsk/tovary_dlya_kompyutera/hp_g6_-_1000_serii_zapchasti_ot_noutbukov_1610580921"}; //ссыли на объявы
+            "https://www.avito.ru/novosibirsk/tovary_dlya_kompyutera/hp_g6_-_1000_serii_zapchasti_ot_noutbukov_1610580921"}; //ссыли на объявы(не мои)
 
     public static void main(String[] args) {
         Document doc = null;
