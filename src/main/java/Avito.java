aea423e4d2f14245ac2d6cf9e0873ce36f803a1a
marckt <marck.for@yandex.ru>
2018-07-11 14:37:55 +0700

    1

--- src/main/java/Avito.java
+++ src/main/java/Avito.java
@@ -11,7 +11,9 @@ public class Avito {
             "https://www.avito.ru/novosibirsk/posuda_i_tovary_dlya_kuhni/tarelka_1398355167",
             "https://www.avito.ru/novosibirsk/tovary_dlya_kompyutera/prodam_otlichnyy_bolshoy_korpus_powerman_pro_pm-003_1649809101",
             "https://www.avito.ru/novosibirsk/tovary_dlya_kompyutera/hp_g6-2000_zapchasti_1342778071",
-            "https://www.avito.ru/novosibirsk/tovary_dlya_kompyutera/hp_g6_-_1000_serii_zapchasti_ot_noutbukov_1610580921"}; //ссыли на объявы(не мои)
+            "https://www.avito.ru/novosibirsk/tovary_dlya_kompyutera/hp_g6_-_1000_serii_zapchasti_ot_noutbukov_1610580921",
+            "https://www.avito.ru/novosibirsk/predlozheniya_uslug/podem_domov_1044779643",
+            "https://www.avito.ru/novosibirsk/predlozheniya_uslug/podnyatie_domov_zamena_rekonstruktsiya_fundamenta_817863747"}; //ссыли на объявы(не мои)
 
     public static void main(String[] args) {
         Document doc = null;
@@ -19,20 +21,27 @@ public class Avito {
         HashMap<String, Integer> sites = new HashMap<>();
         int count = 0;
         for (String s : mySites) {
-            int count2 = 0;
 
             try {
                 doc = Jsoup.connect(s).get();
                 Elements links = doc.select("a[href=#]");
                 count = Integer.parseInt(links.text().substring(links.text().indexOf("+"), links.text().lastIndexOf(")")));
-                count2++;
+                sites.put(s, count);
             } catch (NumberFormatException e) {
                 sites.put(s, 0);
-            } catch (IOException e) {
+            }
+            catch (StringIndexOutOfBoundsException e){
+                sites.put(s,-1);
+            }
+            catch (IOException e) {
                 sites.put(s, -1);
             }
-            sites.put(s, count - count2);
+
+
         }
+        sites.remove(mySites[2]);
+        sites.remove(mySites[3]);
+        sites.remove(mySites[4]);
         printValues(sites);
     }
     private static void printValues(Map<String, Integer> map)
